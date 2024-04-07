2aa76db756a1ddfd696ad4d9cb791dbfda548f69
jamil <jamil.bce@gmail.com>
2024-04-08 02:50:55 +0500

    saved active and recovered values in pandas dataframe with proper names.

--- coronaVirus.py
+++ coronaVirus.py
@@ -28,6 +28,55 @@ if response.status_code == 200:
   recNo = rec[0].find_next_sibling('div', class_='maincounter-number').find('span').text.strip()
   print(f"Recovered: {recNo}")
 
+  # Create a dictionary with the data
+  data = {
+    'active': [None, None, None, None, None],
+    'recovered': [None, None, None, None, None]
+  }
+
+  # Create a DataFrame
+  active_recovered_df = pd.DataFrame(data, index=['actClosedTotal', 'mildRec', 'mildRecPer', 'criticalDeaths', 'criticalDeathsPer'])
+
+  # Rename the DataFrame
+  active_recovered_df.columns = ['active', 'recovered']
+  
+  # Display the DataFrame
+  print(active_recovered_df)
+
+  # Total Active Cases and Closed Cases
+  actClosedTotal = soup.find_all('div', class_='number-table-main')
+  actTotal = actClosedTotal[0].text.strip()
+  active_recovered_df.loc['actClosedTotal', 'active'] = actTotal
+  closedTotal = actClosedTotal[1].text.strip()
+  active_recovered_df.loc['actClosedTotal', 'recovered'] = closedTotal
+  print(active_recovered_df)
+  
+  # in mild condition and Recovered Cases with percentages
+  mildRec = soup.find_all('div', style='float:left; text-align:center')
+  mild = mildRec[0].find('span', class_='number-table').text.strip()
+  active_recovered_df.loc['mildRec', 'active'] = mild
+  mildPer = mildRec[0].find('strong').text.strip()
+  active_recovered_df.loc['mildRecPer', 'active'] = mildPer
+  print(f"{mild} ({mildPer}%)")
+
+  rec = mildRec[1].find('span', class_='number-table').text.strip()
+  active_recovered_df.loc['mildRec', 'recovered'] = rec
+  recPer = mildRec[1].find('strong').text.strip()
+  active_recovered_df.loc['mildRecPer', 'recovered'] = recPer
+  print(active_recovered_df)
+
+  # Critical Cases and deaths with percentages
+  criticalDeaths = soup.find_all('div', style='float:right; text-align:center')
+  critical = criticalDeaths[0].find('span', class_='number-table').text.strip()
+  active_recovered_df.loc['criticalDeaths', 'active'] = critical
+  criticalPer = criticalDeaths[0].find('strong').text.strip()
+  active_recovered_df.loc['criticalDeathsPer', 'active'] = criticalPer
+  deaths = criticalDeaths[1].find('span', class_='number-table').text.strip()
+  active_recovered_df.loc['criticalDeaths', 'recovered'] = deaths
+  deathsPer = criticalDeaths[1].find('strong').text.strip()
+  active_recovered_df.loc['criticalDeathsPer', 'recovered'] = deathsPer
+  print(active_recovered_df)
+
   # Find all table rows with website data (adjust selector as needed)
   table_rows = soup.find_all('table', class_=['wikitable', 'sortable', 'jquery-tablesorter'])
   t_rows = table_rows[0]
